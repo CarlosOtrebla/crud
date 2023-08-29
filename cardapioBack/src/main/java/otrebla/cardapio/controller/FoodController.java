0d466c3f15f841342e5d6aa08cc55a6fea387fab
Carlos Alberto Lopes Duarte <carlosotrebla@gmail.com>
2023-08-29 13:58:08 -0300

    projeto em andamento

---
+++ cardapioBack/src/main/java/otrebla/cardapio/controller/FoodController.java
@@ -0,0 +1,40 @@
+package otrebla.cardapio.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import otrebla.cardapio.food.Food;
+import otrebla.cardapio.food.FoodRepository;
+import otrebla.cardapio.food.FoodRequestDTO;
+import otrebla.cardapio.food.FoodResponseDTO;
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+
+    @Autowired
+    private FoodRepository repository;
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public List<FoodResponseDTO> getAll(){
+
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
+
+    }
+
+}
---
+++ cardapioBack/src/main/java/otrebla/cardapio/food/FoodRequestDTO.java
@@ -0,0 +1,5 @@
+package otrebla.cardapio.food;
+
+public record FoodRequestDTO(String title, String image, Integer price ) {
+
+}
